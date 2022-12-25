8ede391fc8b61d541c35d80788c4afb74e6d4c12
syedsajjadaskari <syedsajjad62@gmail.com>
2022-12-25 16:39:59 +0530

    create project skeleton

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup #return a list of python items(modules/packages)
+from typing import List
+
+
+
+def get_requirements()->List[str]:
+    """This function will return list of requirements"""
+    requirements_list:List[str] = []
+    return requirements_list
+
+setup(
+    name = "sensor",
+    version="0.0.1",
+    author = "syed sajjad askari",
+    author_email = "syedsajjad62@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+
+)
