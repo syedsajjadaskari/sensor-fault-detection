ce15a3d59305f3cd8f0cfbd0e58a76550f101fbf
syedsajjadaskari <syedsajjad62@gmail.com>
2022-12-28 11:20:07 +0530

    modifivation of mongo.db client

--- sensor/configuration/mongo_db_connections.py
+++ sensor/configuration/mongo_db_connections.py
@@ -1,18 +1,27 @@
 import pymongo
 import os
 from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_veriable import MONGODB_URL_KEY
 import certifi
 ca = certifi.where()
 
 class MongoDBClient:
     client = None
-    def __init__(self, database_name=DATABASE_NAME)-> None:
+    def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                #mondo_db_url = "mongodb+srv://user:Atha786@mongodbuniversity.kpjg8sn.mongodb.net/test"
-                MongoDBClient.client = pymongo.MongoClient(os.environ, tlsCAFile=ca)
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                print(mongo_db_url)
+                if "localhost" in mongo_db_url:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
+                else:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
         except Exception as e:
             raise e
+
+
+
+#mondo_db_url = "mongodb+srv://user:Atha786@mongodbuniversity.kpjg8sn.mongodb.net/test"
